1737ab8266631ec277d06bc42ed12163fce3e187
naviadev <qo2oq1020@gmail.com>
2024-06-05 12:55:12 +0900

    XMLRequest TEST

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-const key=  `1pv87H9eg16l2BfHdtOiPxSJpTXetDu9socvkUhDGQW4uLgQYZKHTle4zsfJg8SsbWmX%2BuCJwDfT1WCggv2iDw%3D%3D&`;
+const callAPI = async () => {
+  const key = `key`;
+  const endpoint = `http://apis.data.go.kr/6300000/eventDataService/`;
+  const view = `eventDataListJson?serviceKey=`;
+  const row = `&numOfRows=10&pageNo=1`;
+  const url = endpoint + view + key + row;
+  const xhr = new XMLHttpRequest();
 
-const endpoint =`http://apis.data.go.kr/6300000/eventDataService?servicekey=${key}`
+  await xhr.open("GET", url, true);
 
-console.log(endpoint)
+  xhr.addEventListener('load', async () => {
+    if (xhr.status == 200) {
+      const result = await JSON.parse(xhr.responseText);
+      let main = document.getElementById('main')
+      let list = document.createElement('ul')
+
+      result.msgBody.forEach(element => {
+        let innerList = document.createElement('ul');
+        let attribue = ['beginDt', 'title']
+
+        for (let i = 0; i < attribue.length; i++) {
+          let li = document.createElement('li');
+          li.innerHTML = element[attribue[i]]
+          innerList.appendChild(li);
+        }
+        list.appendChild(innerList)
+      });
+      main.appendChild(list)
+    }
+  })
+  xhr.send()
+}
+
+window.onload = () => {
+  callAPI();
+}
+
+
+
