c18eaaa2a7ceff8123ba5928ecd6a93fe749520b
naviadev <qo2oq1020@gmail.com>
2024-06-05 11:55:31 +0900

    key, endpoint 조립

---
+++ app.js
@@ -0,0 +1,5 @@
+const key=  `1pv87H9eg16l2BfHdtOiPxSJpTXetDu9socvkUhDGQW4uLgQYZKHTle4zsfJg8SsbWmX%2BuCJwDfT1WCggv2iDw%3D%3D&`;
+
+const endpoint =`http://apis.data.go.kr/6300000/eventDataService?servicekey=${key}`
+
+console.log(endpoint)
